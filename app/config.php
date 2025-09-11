d564d9136a060b20667a60dc2f1efb58b0753517
Jitaaa <116050905+Lychtael@users.noreply.github.com>
2025-09-11 15:46:10 +0700

    Delete app/config.php

--- app/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'password');
-define('DB_NAME', 'inventaris_himpunan');
-
-define('BASEURL', 'http://localhost/inventaris-himpunan/public');
